8e02ea8422a493543bdb74b9ba21d3751d2571ea
deworkers <z-be@yandex.ru>
2017-06-30 01:49:22 +0500

    desctop finish

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@
 //= ../plugin/swiper/js/swiper.min.js
 //= ../plugin/jquery.validate.min.js
 //= ../plugin/jquery.waterwheelCarousel.min.js
+//= ../plugin/magnific-popup/jquery.magnific-popup.min.js
 
 /*
  * Custom
--- src/js/part/app.js
+++ src/js/part/app.js
@@ -123,4 +123,31 @@ $(document).ready(function() {
         prevButton: '.swiper-button-prev',
     });
 
+
+    $('.personal-sert-one').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+
+    $('.about-video__preview').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+
+
 });
